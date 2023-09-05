c3e5f405f97664a1e10bcd7b45f902835fdc412c
Brandon <byip2001@gmail.com>
2023-09-05 17:44:59 -0400

    create aws lambda for backend functions

--- js/main.js
+++ js/main.js
@@ -1,26 +1,21 @@
+const API_ENDPOINT = "https://k0v0rx1twg.execute-api.us-east-1.amazonaws.com/getjobs"; // Replace with your Invoke URL
 
-
-const GOOGLE_API = process.env.GOOGLE_API;
-const GOOGLE_ID = process.env.GOOGLE_ID;
-const ADZUNA_ID = process.env.ADZUNA_ID;
-const ADZUNA_KEY = process.env.ADZUNA_KEY;
-
-const client = new GoogleImages(GOOGLE_ID, GOOGLE_API);
 
 window.onload = function() {
     getJobs();
 };
 
 function getJobs() {
-    fetch(`https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${ADZUNA_ID}&app_key=${ADZUNA_KEY}&results_per_page=5&what=software%20engineer&what_and=entry`, {
+    fetch(API_ENDPOINT, {
         method: 'GET',
+
     })
     .then(response => response.json())
     .then(data => {
-        console.log(response.json())
+        console.log(data);
+        // Process the response from Lambda function
     })
     .catch(error => {
-        // handle the error
-        console.log("error")
+        console.error("There was an error calling the Lambda function", error);
     });
 }
