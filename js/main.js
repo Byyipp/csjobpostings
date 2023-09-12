b58d1e7514b1a224c3d95e402731898dfb02ed14
Brandon <byip2001@gmail.com>
2023-09-12 19:49:26 -0400

    html updates

--- js/main.js
+++ js/main.js
@@ -2,20 +2,24 @@ const API_ENDPOINT = "https://k0v0rx1twg.execute-api.us-east-1.amazonaws.com/get
 
 
 window.onload = function() {
-    getJobs();
+    getJobs().then(jobDataMap => {
+        if (jobDataMap) {
+            updateJobPostings(jobDataMap);
+        }
+    });
 };
 
 function getJobs() {
-    fetch(API_ENDPOINT, {
+    return fetch(API_ENDPOINT, {
         method: 'GET',
     })
-    .then(response => response.json())  // Convert the response to JSON
+    .then(response => response.json())
     .then(data => {
-        console.log("Parsed Lambda Response:", data);  // Log the initial response
-    
-        // If the 'body' attribute of your Lambda response contains stringified JSON:
+        // console.log("Parsed Lambda Response:", data);
+
         const jobData = JSON.parse(data.body);
-        const desiredData = jobData.results.map(job => ({
+        const jobDataMap = jobData.results.map(job => ({
+            company_image: null,
             title: job.title,
             company_display_name: job.company.display_name,
             location_display_name: job.location.display_name.slice(0, job.location.display_name.indexOf(",")),
@@ -25,11 +29,72 @@ function getJobs() {
             redirect_url: job.redirect_url
         }));
 
-        console.log(desiredData);
-    
-        // Further processing of actualData if needed...
+        console.log(jobDataMap);
+        return jobDataMap;
     })
     .catch(error => {
         console.error("There was an error calling the Lambda function", error);
+        return null;
+    });
+}
+
+
+function updateJobPostings(jobDataMap) {
+    const jobContainer = document.querySelector('[role="list"]');
+
+    // Clear existing job cards
+    const oldJobCards = document.querySelectorAll('.job-card-list');
+    oldJobCards.forEach(card => card.remove());
+
+    // Generate new job cards based on the job data map
+    jobDataMap.forEach(job => {
+        const jobCard = document.createElement('li');
+        jobCard.className = 'job-card-list';
+
+        const jobList = document.createElement('ul');
+        jobList.className = 'job-list w-list-unstyled';
+
+        // Title
+        const titleHeader = document.createElement('h3');
+        titleHeader.className = 'job-title-header';
+        titleHeader.innerText = job.title;
+
+        // Company
+        const companyHeader = document.createElement('h4');
+        companyHeader.className = 'company-header';
+        companyHeader.innerText = job.company_display_name;
+
+        // Location - Display name
+        const locationHeader = document.createElement('h4');
+        locationHeader.className = 'location-header';
+        locationHeader.innerText = job.location_display_name;
+
+        // Location - Area
+        const locationState = document.createElement('h5');
+        locationState.className = 'location-state';
+        locationState.innerText = job.location_area;
+
+        // Salary
+        const salaryText = document.createElement('strong');
+        salaryText.className = 'salary-money-text';
+        salaryText.innerText = `$${job.salary_min.toFixed(2)}`;
+
+        // Button
+        const viewJobButton = document.createElement('a');
+        viewJobButton.className = 'view-job-button w-button';
+        viewJobButton.href = job.redirect_url;
+        viewJobButton.innerText = 'View Job';
+
+        // Append elements to the job card
+        jobList.appendChild(titleHeader);
+        jobList.appendChild(companyHeader);
+        jobList.appendChild(locationHeader);
+        jobList.appendChild(locationState);
+        jobList.appendChild(salaryText);
+        jobList.appendChild(viewJobButton);
+        jobCard.appendChild(jobList);
+
+        // Append the job card to the container
+        jobContainer.appendChild(jobCard);
     });
 }
